785dcec2109c8055310cbc1614b665689b7559b2
Erika <a01745158@itesm.mx>
2022-10-28 09:58:27 -0500

    final

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const fileUpload = require('express-fileupload');
 const modeloIARoutes = require('./routes/modeloIA');
 const ejemploBDRoutes = require('./routes/ejemploBD')
 const sequelize = require('./utils/database')
+const cors = require('cors')
     //middlewares funcionales
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public'))) // Todo lo que está en public va a aparecer como servicios o peticiones
